7e3e3e954b92f71ff50820ea3cee61c227e4b93c
Deeksha Reddy <dpr5ks@labmini12.lab.cs.virginia.edu>
2017-09-17 14:41:36 -0400

    Initial Commit

---
+++ iosminiappTests/iosminiappTests.swift
@@ -0,0 +1,36 @@
+//
+//  iosminiappTests.swift
+//  iosminiappTests
+//
+//  Created by Deeksha Reddy on 9/17/17.
+//  Copyright © 2017 Deeksha Reddy. All rights reserved.
+//
+
+import XCTest
+@testable import iosminiapp
+
+class iosminiappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
